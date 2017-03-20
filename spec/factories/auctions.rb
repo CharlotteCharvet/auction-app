cfcfff87e582aebce863529de757a72bee2ac169
CharlotteCharvet <charlotte.lannon@gmail.com>
2017-03-20 15:48:59 +0800

    auction table and validations up to date

---
+++ spec/factories/auctions.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :auction do
+    starts_at "2017-03-20 14:34:30"
+    ends_end "2017-03-20 14:34:30"
+    time_zone_id "MyString"
+    physical_address "MyText"
+    name "MyString"
+    donation_window_ends_at "2017-03-20 14:34:30"
+  end
+end
---
+++ spec/models/auction_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Auction do
+
+describe "attributes" do  
+  it {is_expected.to have_attribute :starts_at }
+  it {is_expected.to have_attribute :ends_at }
+  it {is_expected.to have_attribute :time_zone_id }
+  it {is_expected.to have_attribute :physical_address }
+  it {is_expected.to have_attribute :donation_window_ends_at}
+end
+
+describe "validations" do
+  it "ends_at is after starts_at" do
+    subject.ends_at = DateTime.new(2017,03,20,0,0)
+    subject.starts_at = subject.ends_at.advance(seconds: 1)
+    subject.valid?
+    expect(subject.errors[:ends_at]).to include "must be greater than starts_at"
+  end
+
+  it "starts_at is after donation_window_ends_at" do
+    subject.starts_at = DateTime.new(2017,03,20,0,0)
+    subject.donation_window_ends_at = subject.starts_at.advance(seconds: 1)
+    subject.valid?
+    expect(subject.errors[:starts_at]).to include "must be greater than donation_window_ends_at"
+  end
+
+  it "expect time_zone_id to be a valid time_zone" do
+    subject.time_zone_id = "MarsTime"
+    subject.valid?
+    expect(subject.errors[:time_zone_id]).to include "is not included in the list"
+  end
+end
+end
